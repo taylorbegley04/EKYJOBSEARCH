15ade0d47c46f4df4e51b71e9400947cc1d92256
taylorbegley04 <brewer.taylor13@yahoo.com>
2017-03-31 14:47:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+$('#jobseekerbutton').hover(function(){
+    $('#jobseekerbutton').css('font-size', "30px");
+    });
+$('#compbutton').hover(function(){
+    $('#compbutton').css('font-size', "30px");
+    });
 
+
+$('#jobseekerbutton').mouseleave(function(){
+    $('#jobseekerbutton').css('font-size', "17px");
+    });
+$('#compbutton').mouseleave(function(){
+    $('#compbutton').css('font-size', "17px");
+    });
+   
